9f96f360f0c1080f050a4a971b4b002edcac8e80
Ayush Jangid <ayushjangid441@gmail.com>
2023-11-24 17:04:35 +0530

    user controller updated

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,50 +1,110 @@
-// const urserData=require("../router/userRoutes")
-const User=require("../model/userModel")
+const User = require("../model/userModel");
 
-const login=async(req,res)=>{
-    res.send("logining data.....")
-}
-const signup=async(req,res)=>{
-    // res.send("signing up....")json??
-    const {contactNumber,password,fullname,email}=req.body;
-    const data=await User.create({contactNumber,password,fullname,email})
-    res.json(data);
-    //
-}
+const login = async (req, res) => {
+  res.send("logining data.....");
+};
+const signup = async (req, res) => {
+  try {
+    const { contactNumber, password, fullname, email } = req.body;
+    const user = await User.create({
+      contactNumber,
+      password,
+      fullname,
+      email,
+    });
+    return res.status(200).json({ success: true, user });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-const deleteProfile=async(req,res)=>{
-    // res.send("deleting profile  data.....")
-    const _id=req.body._id
-    const userToDelete=await User.findOneAndDelete({_id})
-    res.json(userToDelete)
+const deleteProfile = async (req, res) => {
+  try {
+    const _id = req.body._id;
+    if (!_id) {
+      return res
+        .status(400)
+        .json({ message: "Please provide Id", success: false });
+    }
+    const result = await User.findOneAndDelete({ _id });
+    if (!result) {
+      return res
+        .status(404)
+        .json({ message: "User not found", success: false });
+    }
+    res.status(200).json({ result, success: true });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-}
+const changeRole = async (req, res) => {
+  try {
+    const _id = req.body._id;
+    if (!_id) {
+      return res
+        .status(400)
+        .json({ message: "Please provide Id", success: false });
+    }
+    const user = await User.findOneAndUpdate(
+      { _id },
+      {
+        role: req.body.role,
+      }
+    );
+    res.status(200).json({ user, success: true });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+const getProfile = async (req, res) => {
+  try {
+    const _id = req.body._id;
+    if (!_id) {
+      return res
+        .status(400)
+        .json({ message: "Please provide Id", success: false });
+    }
+    const user = await User.findOne({ _id });
+    if (!user) {
+      return res.status(404).json({ msg: "User not found", success: false });
+    }
+    return res.status(200).json({ user, success: true });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-const changeRole=async(req,res)=>{
-    // res.send("changing role.....")
-    const _id=req.body._id
-    //id 6560714908dcca74a600ac84...fetch user ..update user 
-    const userRole=await User.findOneAndUpdate({_id},{
-        role:req.body.role
-    })
-    res.send(userRole)
-
-}
-const getProfile=async(req,res)=>{
-    // res.send("getting profile data.....")
-    const _id=req.body._id
-    const data=await User.findOne({_id})
-    res.json(data)
-}
-
-const updateProfile=async(req,res)=>{
-    // res.send("updating profile...")
-    const _id=req.body._id;
-    const UpdatedUser=await User.findOneAndUpdate({_id},{
-        email:req.body.email,
-        fullname:req.body.fullname,
-    })
-    res.json(UpdatedUser)
-}
-module.exports={login,signup,deleteProfile,changeRole,getProfile,updateProfile}
- 
+const updateProfile = async (req, res) => {
+  try {
+    const _id = req.body._id;
+    if (!_id) {
+      return res
+        .status(400)
+        .json({ message: "Please provide Id", success: false });
+    }
+    const updatedUser = await User.findOneAndUpdate(
+      { _id },
+      {
+        email: req.body.email,
+        fullname: req.body.fullname,
+      }
+    );
+    if (!updatedUser) {
+      return res
+        .status(404)
+        .json({ message: "User not found", success: false });
+    }
+    return res.status(200).json({ updatedUser, success: true });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+module.exports = {
+  login,
+  signup,
+  deleteProfile,
+  changeRole,
+  getProfile,
+  updateProfile,
+};
