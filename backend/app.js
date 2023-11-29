a8c35410e9aca8847f034cb817c4f2200dd4345f
Ayush Jangid <ayushjangid441@gmail.com>
2023-11-29 23:33:07 +0530

    payment functionality added

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const connectDB = require("./db/db")
+const cors = require("cors");
 const app = express()
 app.use(express.json())
+app.use(cors());
 
 const userRouter=require("./router/userRoutes")
 const courseRouter=require("./router/courseRouter")
+const paymentRouter = require("./router/paymentRouter");
+
+app.use("/api/v1/payment", paymentRouter);
 app.use("/api/v1/auth",userRouter)
 app.use("/api/v1",courseRouter)
 
--- backend/controller/courseController.js
+++ backend/controller/courseController.js
@@ -1,55 +1,140 @@
-const course = require("../model/courseModel")
+const Course = require("../model/courseModel");
 const createCourse = async (req, res) => {
-  const { createrName, category, poster, lectures, description, title } = req.body
-  const Singlecourse = await course.create({ createrName, category, poster, lectures, description, title })
-  res.json(Singlecourse)
-
+  try {
+    const { createrName, category, poster, lectures, description, title } =
+      req.body;
+    if (
+      !createrName ||
+      !category ||
+      !poster ||
+      !lectures ||
+      !description ||
+      !title
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Please Provide proper Information", success: false });
+    }
+    const createdCourse = await Course.create({
+      createrName,
+      category,
+      poster,
+      lectures,
+      description,
+      title,
+    });
+    res.status(200).json({ createdCourse, success: true });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
 };
 
 const addLectures = async (req, res) => {
-  const { video, description, title, _id } = req.body
-  const updatedCourse = await course.findOneAndUpdate({ _id }, {
-    $push: {
-      lectures: {
-        title,
-        description,
-        video
-      }
+  try {
+    const { video, description, title, _id } = req.body;
+    if (!video || !description || !title || !_id) {
+      return res
+        .status(400)
+        .json({ message: "Please Provide proper Information", success: false });
     }
-  }, {
-    new: true
-  })
-  res.json(updatedCourse)
-  console.log("lect is added")
+    const updatedCourse = await Course.findOneAndUpdate(
+      { _id },
+      {
+        $push: {
+          lectures: {
+            title,
+            description,
+            video,
+          },
+        },
+      },
+      {
+        new: true,
+      }
+    );
+    if (!updatedCourse) {
+      return res
+        .status(404)
+        .json({ message: "Course Not Found", success: false });
+    }
+    return res.status(200).json({ updatedCourse, success: false });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
 };
 
-const getAllCourses =async (req, res) => {
-  const allCourses=await course.find({})
-  res.json(allCourses)
+const getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find({});
+    return res.status(200).json({ courses, success: true });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
 };
 
 const getCourseLect = (req, res) => {
   res.send("getCourseLect...");
 };
-const deleteCourse = async(req, res) => {
-  const {_id}= req.body
-  const Deletedcourse = await course.findByIdAndDelete(_id)
-  res.json(Deletedcourse)
+const deleteCourse = async (req, res) => {
+  try {
+    const { _id } = req.body;
+    if (!_id) {
+      return res
+        .status(400)
+        .json({ message: "Please provide courseId", success: false });
+    }
+    const deletedcourse = await Course.findByIdAndDelete(_id);
+    if (!deletedcourse) {
+      return res
+        .status(404)
+        .json({ message: "Course Not Found", success: false });
+    }
+    return res.status(200).json({ deletedcourse, success: true });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
 };
 
-const deleteLect = async(req, res) => {
-  const {_id,lect_id}=req.body
-  const updatedCourse = await course.findOneAndUpdate({ _id }, {
-    $pull: {
-      lectures: {
-        _id:lect_id
-      }
+const deleteLect = async (req, res) => {
+  try {
+    const { _id, lect_id } = req.body;
+    if (!_id || !lect_id) {
+      return res
+        .status(400)
+        .json({ message: "Please Provide proper Information", success: false });
     }
-  }, {
-    new: true
-  })
-  res.json(updatedCourse)
-  console.log("lecture is deleted")
+    const updatedCourse = await Course.findOneAndUpdate(
+      { _id },
+      {
+        $pull: {
+          lectures: {
+            _id: lect_id,
+          },
+        },
+      },
+      {
+        new: true,
+      }
+    );
+    if (!updatedCourse) {
+      return res
+        .status(404)
+        .json({ message: "Course Not Found", success: false });
+    }
+    return res.status(200).json({ updatedCourse, success: false });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal Server Error", success: false });
+  }
 };
 
 module.exports = {
